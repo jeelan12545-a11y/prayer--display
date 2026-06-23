f368a03904cad30e3093271326d7e43bd94d524b
jeelan12545-a11y <jeelan.12545@gmail.com>
2026-06-23 20:48:57 +0400

    Add top section structure Arabic text in top section of App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,17 @@ import "./index.css";
 function App() {
   return (
     <div className="display-page">
-      <div className="display-frame">
+      <div className="top-section">
+
+  <div className="arch-section">
+
+    <div className="takbeer">
+      الله أكبر
+    </div>
+
+  </div>
+
+</div>
 
       </div>
     </div>
